21cbe85a736d020ea571f7ea2a253f5c457e4d69
Toni  Diep <adiep123@gmail.com>
2020-04-02 02:00:26 +0000

    Automatically backed up by Learn

--- lib/my_all.rb
+++ lib/my_all.rb
@@ -1,9 +1,9 @@
+require 'pry'
 
-
-def my_all?([1,2,3])
+def my_all?(collection)
   i = 0
-  while i < 3
-    yield(1)
-    i = 0 + 1
+  while i <= collection.length
+    yield(collection[i])
+    i = i + 1
   end
 end
